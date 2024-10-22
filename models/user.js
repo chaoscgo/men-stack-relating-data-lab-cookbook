e1dc07d60dfa9c46f22b075c084fae7effeffa74
Beverly <chaoscgo@hotmail.com>
2024-10-22 11:17:36 -0500

    added foodSchema, embedded it in userSchema, built foodsController, added middleware, built nav partial

--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+  food: {type: String, required: true},
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,8 +13,12 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  pantry: [foodSchema],
 });
 
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
+
+
