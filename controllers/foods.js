ce2f417671b32b93d890337445a6b6b7600189d3
Beverly <chaoscgo@hotmail.com>
2024-10-28 09:48:10 -0500

    added delete route and button

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+// Index Route
 router.get('/', async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
@@ -14,11 +15,13 @@ router.get('/', async (req, res) => {
     res.redirect('/');
   }
   });
-  
+
+// New Route  
 router.get('/new', async (req, res) => {
     res.render('foods/new.ejs');
 });
 
+// Create Route
 router.post('/', async (req, res) => {
   try {
   const currentUser = await User.findById(req.session.user._id);
@@ -31,5 +34,18 @@ router.post('/', async (req, res) => {
     res.redirect('/');
   }
 })
+
+// Delete Route
+router.delete('/:foodId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.foods.id(req.params.foodId).deleteOne();
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/foods`);
+  } catch(error) {
+    console.log(error);
+    res.redirect('/');
+  }
+  });
 
 module.exports = router;
